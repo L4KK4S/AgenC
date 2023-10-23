227942119eccf9df6b1edfc95451019f69d34e23
L4KK4S <abdel-waheb.sakkal@efrei.net>
2023-10-23 18:22:36 +0200

    Update list.c/h cell.c/h

--- cell.c
+++ cell.c
@@ -1,1 +1,18 @@
 #include "cell.h"
+#include <stdlib.h>
+
+p_cell createEmptyCell(int x) {
+    p_cell new = (p_cell) malloc (sizeof(t_cell));
+    new->value=x;
+    new->next_h=NULL;
+    new->next_v=NULL;
+    return new;
+}
+
+void addVtab(p_cell cell, int x) {
+    p_cell tmp = cell;
+    for (int i = 0 ; i < x ; i++) {
+        tmp->next_v = createEmptyCell(cell->value);
+        tmp=tmp->next_v;
+    }
+}
--- list.c
+++ list.c
@@ -1,11 +1,17 @@
 #include "list.h"
 #include <stdlib.h>
 
-p_list createEmptylist() {
+p_list createEmptylist(int x) {
     p_list new = (p_list) malloc (sizeof(t_list));
+    new->level=x;
     new->head_h=NULL;
     new->head_v=NULL;
     return new;
 }
 
+void display_list (p_list list) {
+    p_cell tmp = list->head_v;
+    (while tmp!=NULL) {
 
+    }
+}
