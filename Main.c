19c934fd75bf24c78a45afaa5f1f4057ee1bae40
Your Name <maryam22georgi@gmail.com>
2023-11-01 17:20:13 +0100

    Add change_to_min

--- Main.c
+++ Main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include "list.h"
 #include "list.c"
+#include <stdlib.h>
+#include <string.h>
 int main()
 {
-    printf("%d\n", comparerString1("thomas", "thomas"));
+   // printf("%d\n", comparerString1("thomas", "thomas"));
     printf("%d\n", comparerString1("Thomas", "thomas"));
-    printf("%d\n", comparerString1("Maryam", "thomas"));
-    printf("%d\n", comparerString1("thomas", "Maryam"));
-    printf("%d\n", comparerString1("thomas", "thomas2"));
+    //printf("%d\n", comparerString1("Maryam", "thomas"));
+    //printf("%d\n", comparerString1("thomas", "Maryam"));
+    //printf("%d\n", comparerString1("thomas", "thomas2"));
+    char *string2 = malloc(30); // Chaîne dynamique
 
-
+    strcpy(string2, "Autre Chaine De Test");
+    change_maj_to_min(string2);
 
     return 0;
 }
--- list.c
+++ list.c
@@ -5,32 +5,29 @@
 #include "list.h"
 #include <stdio.h>
 #include <string.h>
-int compareStrings( char *a, char *b) {
-    while (*a != '\0' && *b != '\0') {
-        if (*a < *b) {
-            return -1; //string1 is shorter than string2
-        } else if (*a > *b) {
-            return 1; //  string1 is greater than string2
+#include <ctype.h>
+#include <stdlib.h>
+
+/*void change_maj_to_min( char *s) {
+   for (int i = 0; s[i] != '\0'; i++) {
+       if (s[i] >= 'A' && s[i] <= 'Z') {
+        s[i] = s[i] + 32; // Conversion en minuscule
         }
-        a++;
-        b++;
+
     }
-    if (*a == '\0' && *b == '\0'){
-        return 0; // Both strings are identical
-    }
-    if (*a == '\0') {
-        return -1; //string1 is shorter than string2
-    }
-    else {
-        return 1; // string1 is greater than string2
-    }
-}
+ */
+  void change_maj_to_min( char *str) {
+      int i;
+      for (i = 0; str[i] != '\0'; i++) {
+          str[i] = tolower(str[i]);
+      }
+  }
 int comparerString1( char *s1, char *s2) {
     int i = 0;
     int n1 = strlen(s1);
     int n2 = strlen(s2);
-
-
+    change_maj_to_min(s1);
+    change_maj_to_min(s2);
     while (i < n1 && i < n2 && s1[i] == s2[i]) {     // Compare characters until a mismatch is found or the end of either string is reached
         i++;
     }
