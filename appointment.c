2e50dec4cb9fdd1aac04557048cc1dffc43b0326
thomas <amiscremail@gmail.com>
2023-11-01 13:08:59 +0100

    save to check if maryam commit work

--- appointment.c
+++ appointment.c
@@ -10,6 +10,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "appointment.h"
 
 // -------------------------- Functions --------------------------
@@ -23,21 +24,38 @@ p_appointment createAppointmentCell () {
     new ->time = 0;
     return new;
 }
+
 int checkDateFormat(char* input, p_appointment new_appointment) {
-    char
+    char day[2], month[2], years[4];
+    char temp[10] = " ", reset[10] = " ";
     int i = 0;
     if (input[10]=='\0') {                                                 // Check if the input is the correct length
         while (input[i] != '\0') {
-            if ((i == 2 || i == 5) &&
-                input[i] != '/') {                                         // Check that '/' are in the good positions
+            if ((i == 2 || i == 5) && input[i] != '/') {                                         // Check that '/' are in the good positions
                 return -1;
             } else {
                 if (input[i] < 48 || input[i] > 57) {                      // String contain caracters that aren't numbers
                     return -1;
                 }
             }
+            strcpy(temp, reset);
+            temp[0] = input[i];
+            if (i<2) {
+                strcat(day, temp);
+            } else if (i>2 && i<5) {
+                strcat(month, temp);
+            } else if (i > 5 && i <10) {
+                strcat(years, temp);
+            }
             i++;
         }
+        printf("correct entry, let's check if the date is possible\n");
+        new_appointment->date.day = atoi(day);
+        new_appointment->date.month = atoi(month);
+        new_appointment->date.years = atoi(years);
+
+
+
     } else {
         return -1;
     }
