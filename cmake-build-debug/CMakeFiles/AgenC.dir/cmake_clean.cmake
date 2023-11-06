3f8e379b6036c5e4fd178d5b27d0ddb167b8c42d
L4KK4S <abdel-waheb.sakkal@efrei.net>
2023-11-06 19:33:41 +0100

    Merge of Thoma's branch

---
+++ cmake-build-debug/CMakeFiles/AgenC.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "AgenC.exe"
+  "AgenC.exe.manifest"
+  "AgenC.pdb"
+  "CMakeFiles/AgenC.dir/Main.c.obj"
+  "CMakeFiles/AgenC.dir/Main.c.obj.d"
+  "CMakeFiles/AgenC.dir/appointment.c.obj"
+  "CMakeFiles/AgenC.dir/appointment.c.obj.d"
+  "CMakeFiles/AgenC.dir/cell.c.obj"
+  "CMakeFiles/AgenC.dir/cell.c.obj.d"
+  "CMakeFiles/AgenC.dir/contact.c.obj"
+  "CMakeFiles/AgenC.dir/contact.c.obj.d"
+  "CMakeFiles/AgenC.dir/list.c.obj"
+  "CMakeFiles/AgenC.dir/list.c.obj.d"
+  "CMakeFiles/AgenC.dir/menu.c.obj"
+  "CMakeFiles/AgenC.dir/menu.c.obj.d"
+  "CMakeFiles/AgenC.dir/timer.c.obj"
+  "CMakeFiles/AgenC.dir/timer.c.obj.d"
+  "libAgenC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AgenC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
