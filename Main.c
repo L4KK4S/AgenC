af064b201a6cc7b4c75b0c17bad9556c11c4b33e
thomas <amiscremail@gmail.com>
2023-11-13 11:53:52 +0100

    Menu created with search, contact creation and show list function and create appointment

--- Main.c
+++ Main.c
@@ -37,54 +37,13 @@ int main() {
     // Test list
     p_contact_list test = createExempleList2(0);
 
-    // Test for the create Appointment function
-    /*p_appointment new = createAppointment(test);
-    printf("%d %d %d\n", new->date.day, new->date.month, new->date.years);
-    printf("%dh%d\n", new->hour.hours, new->hour.minutes);
-    printf("%dh%d\n", new->length.hours, new->length.minutes);
-    printf("%s\n", new->object);*/
-
-    // Test for the completion + checkname + search contact
-
-    /*p_contact tmp = test->levels[0];
-    while (tmp!=NULL) {
-        printf("%s\n",unformatString(tmp->name));
-        tmp = tmp->levels[0];
-    }
-    char* name = (char*) malloc (sizeof(char));
-    do {
-        printf("Auto completion is enable on this entry, type help to know more about how it work\n");                                       // Message to indicate the entry button are differnet than other entries
-        name = autoCompletion(test);
-    } while (name==NULL);
-    printf("%s\n", name);*/
-
-    // Test for create appointment list
-
-//    p_appointment new = (p_appointment) malloc (sizeof(p_appointment));
-//    new->object = "test";
-//    new->length.hours=12;
-//    new->length.minutes=12;
-//    new->hour.hours = 12;
-//    new->hour.minutes = 12;
-//    new->date.day = 12;
-//    new->date.month = 9;
-//    new->date.years = 2004;
-//    testPrintAppointment(new);
-//    insertAppointment(searchContact("picou_thomas", test), new);
-//   uniform_display_contact_list(test);
-
-    /*while (1) {
-
-        createAppointment(test);
-    }*/
-
     // ------------------------- Variable definition --------------------------
 
     char* input = (char*) malloc(100*sizeof(char));
     char* argument = (char*) malloc (100*sizeof(char));
     p_contact_list contactList = createEmptyList();
     p_contact new;
-    int exit = 0, function = -0;
+    int exit = 0, function;
 
     // ------------------------- Test list definition --------------------------
 
