f5f27694bb655df3077d39c769951bcd98b62006
thomas <amiscremail@gmail.com>
2023-11-16 09:58:31 +0100

    Search dtc commented + loop for the insert dtc

--- contact.c
+++ contact.c
@@ -368,21 +368,53 @@ void insertContact(p_contact_list list, p_contact new) {
 }
 
 int searchContact_dtc(p_contact_list list, char* search) {
+    int current_level = 3;                                                                                                      // Set first level to 3 (max level)
+    p_contact tmp = list->levels[current_level];                                                                                // Create a cursor
+    p_contact prev = tmp;                                                                                                       // Create a prev
+    while ((tmp!=NULL || current_level!=0) && (current_level!=0 || compareString(tmp->name, search)==1)) {       // Loop to continue while it's not level 0 and NULL or level 0 and next superior
+        if (tmp==NULL && current_level!=0) {                                                                                    // Check if we have to go down a level
+            tmp = prev;                                                                                                         // Set the tmp to the last cell not NULL
+            current_level--;                                                                                                    // Down the level
+        } else if (compareString(tmp->name, search)==0) {                                                         // Check if we have found the value
+            //printf("tmp : %s, level : %d\n", tmp->name, current_level);
+            return 1;
+        } else {                                                                                                                // If we can still go fw we increment
+            if (compareString(tmp->name, search)== -1 && current_level!=0) {                                      // Check if the next is superior to the search contact
+                //printf("tmp : %s, level : %d\n", tmp->name, current_level);
+                tmp = prev;                                                                                                     // If yes we down a level
+                current_level--;
+            } else {                                                                                                            // if not we go to the next cell
+                //printf("tmp : %s, level : %d\n", tmp->name, current_level);
+                prev = tmp;
+                tmp = tmp->levels[current_level];
+            }
+        }
+    }
+    return 0;                                                                                                                   // If we haven't find the value we return 0
+}
+
+void insertContact_dtc(p_contact_list list, char* search) {
     int current_level = 3;                                                                                              // Set first level to 3 (max level)
     p_contact tmp = list->levels[current_level];                                                                        // Create a cursor
     p_contact prev = tmp;                                                                                               // Create a prev
-    while (tmp!=NULL || current_level!=0) {                                                                             // Loop to go through all cell  of level 0 at worst case
+    while ((tmp!=NULL || current_level!=0) && (current_level!=0 || compareString(tmp->name, search)==1)) {                                                                             // Loop to go through all cell  of level 0 at worst case
         if (tmp==NULL && current_level!=0) {                                                                            // Check if we have to go down a level
             tmp = prev;                                                                                                 // Set the tmp to the last cell not NULL
             current_level--;                                                                                            // Down the level
-        } else if (compareString(tmp->name, search)==0) {                                                 // Check if we have found the value
-            return 1;
+        } else if (compareString(tmp->name, search)==0 && current_level==0) {                                                 // Check if we have found the value
+            printf("tmp : %s, level : %d\n", tmp->name, current_level);
         } else {                                                                                                        // If we can still go fw we increment
-            prev = tmp;
-            tmp = tmp->levels[current_level];
+            if (compareString(tmp->name, search)!=1 && current_level!=0) {
+                printf("tmp : %s, level : %d\n", tmp->name, current_level);
+                tmp = prev;                                                                                                 // Set the tmp to the last cell not NULL
+                current_level--;
+            } else {
+                printf("tmp : %s, level : %d\n", tmp->name, current_level);
+                prev = tmp;
+                tmp = tmp->levels[current_level];
+            }
         }
     }
-    return 0;                                                                                                           // If we haven't find the value we return 0
 }
 
 
