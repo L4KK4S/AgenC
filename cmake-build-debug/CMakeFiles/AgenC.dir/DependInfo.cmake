4e19a2f2d3f882eabf33f3b97dc8969984587407
thomas <amiscremail@gmail.com>
2023-12-10 17:49:53 +0100

    fix of the menu function

---
+++ cmake-build-debug/CMakeFiles/AgenC.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/thomas/CLionProjects/AgenC/Main.c" "CMakeFiles/AgenC.dir/Main.c.o" "gcc" "CMakeFiles/AgenC.dir/Main.c.o.d"
+  "/Users/thomas/CLionProjects/AgenC/appointment.c" "CMakeFiles/AgenC.dir/appointment.c.o" "gcc" "CMakeFiles/AgenC.dir/appointment.c.o.d"
+  "/Users/thomas/CLionProjects/AgenC/cell.c" "CMakeFiles/AgenC.dir/cell.c.o" "gcc" "CMakeFiles/AgenC.dir/cell.c.o.d"
+  "/Users/thomas/CLionProjects/AgenC/contact.c" "CMakeFiles/AgenC.dir/contact.c.o" "gcc" "CMakeFiles/AgenC.dir/contact.c.o.d"
+  "/Users/thomas/CLionProjects/AgenC/file.c" "CMakeFiles/AgenC.dir/file.c.o" "gcc" "CMakeFiles/AgenC.dir/file.c.o.d"
+  "/Users/thomas/CLionProjects/AgenC/list.c" "CMakeFiles/AgenC.dir/list.c.o" "gcc" "CMakeFiles/AgenC.dir/list.c.o.d"
+  "/Users/thomas/CLionProjects/AgenC/menu.c" "CMakeFiles/AgenC.dir/menu.c.o" "gcc" "CMakeFiles/AgenC.dir/menu.c.o.d"
+  "/Users/thomas/CLionProjects/AgenC/timer.c" "CMakeFiles/AgenC.dir/timer.c.o" "gcc" "CMakeFiles/AgenC.dir/timer.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/AgenC.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "AgenC"
+  "AgenC.pdb"
+  "CMakeFiles/AgenC.dir/Main.c.o"
+  "CMakeFiles/AgenC.dir/Main.c.o.d"
+  "CMakeFiles/AgenC.dir/appointment.c.o"
+  "CMakeFiles/AgenC.dir/appointment.c.o.d"
+  "CMakeFiles/AgenC.dir/cell.c.o"
+  "CMakeFiles/AgenC.dir/cell.c.o.d"
+  "CMakeFiles/AgenC.dir/contact.c.o"
+  "CMakeFiles/AgenC.dir/contact.c.o.d"
+  "CMakeFiles/AgenC.dir/file.c.o"
+  "CMakeFiles/AgenC.dir/file.c.o.d"
+  "CMakeFiles/AgenC.dir/list.c.o"
+  "CMakeFiles/AgenC.dir/list.c.o.d"
+  "CMakeFiles/AgenC.dir/menu.c.o"
+  "CMakeFiles/AgenC.dir/menu.c.o.d"
+  "CMakeFiles/AgenC.dir/timer.c.o"
+  "CMakeFiles/AgenC.dir/timer.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AgenC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
